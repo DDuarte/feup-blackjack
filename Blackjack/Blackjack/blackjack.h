257d9e475d31d1dd39618990fe69b0510db6c8c2
Miguel Marques <migulorama@gmail.com>
2012-04-18 00:55:04 +0100

    declarationo of  player* queue and player* array on blackjack.h

--- Blackjack/Blackjack/blackjack.h
+++ Blackjack/Blackjack/blackjack.h
@@ -5,6 +5,9 @@
 
 #include <vector>
 #include <queue>
+
+const unsigned int NUM_ACTIVE_PLAYERS= 4;
+
 class BlackJack : public Game
 {
 public:
@@ -13,7 +16,10 @@ private:
     std::vector<Player> _players;
     std::queue<*Player> _waitingPlayers;
 
+    Player* _activePlayers[NUM_ACTIVE_PLAYERS];
 
+    Dealer _dealer;
+    Deck _deck;
 };
 
 #endif // BLACKJACK_H
