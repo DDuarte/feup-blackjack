8b6ede591135670b7086f19a1cb2fc21ff6859f2
Miguel Marques <migulorama@gmail.com>
2012-04-17 21:12:09 +0100

    Creation of the Game header file and declaration of corresponding class. Declaration of the Player class.

---
+++ Blackjack/Blackjack/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+public:
+private:
+
+};
+
+#endif // GAME_H
--- Blackjack/Blackjack/player.h
+++ Blackjack/Blackjack/player.h
@@ -1,10 +1,29 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
+#include <vector>
+
 class Player
 {
 public:
+	Player();
+	Player(std::string name, double balance, bet);
+
+	std::string GetName() const { return _name; };
+	double GetBalance() const { return _balance; };
+	double GetBet() const { return _bet; };
+	std::vector<Card> GetCards() const { return _cards; };
+
+	void SetName(std::string name) { (name.size() > 0 ? _name = name : void); };
+	void SetBalance(double balance) { _balance = balance; };
+	bool Bet(double amount);	// Make a Bet.Returns true if player has money to bet amount or retuns false otherwise
+
 private:
+	std::string _name;
+	double _balance;
+	double _bet;
+	std::vector<Card> _cards;
 };
 
 #endif // PLAYER_H
